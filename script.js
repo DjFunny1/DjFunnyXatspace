bf1cf333ce6a7aa697829156788633fadb3c3eb7
Ronald Chacon <166889445+DjFunny1@users.noreply.github.com>
2025-03-22 19:58:28 -0400

    Update script.js [pin validator]

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function toggleDarkMode() {
     document.body.classList.toggle("dark-mode");
 }
+function unlockProfile() {
+    var pin = document.getElementById("pin-input").value;
+    if (pin === "1989") {
+        document.getElementById("lock-screen").style.display = "none";}}
